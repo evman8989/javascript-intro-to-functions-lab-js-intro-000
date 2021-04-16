43db01aff1213cf96fe369141d58a394c8626c79
chatty-forum-3938 <evanshepard1208@gmail.com>
2021-04-16 12:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   } else if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
-  } else {
-    return "I can\'t hear you!"
-  }
-  if (string === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
