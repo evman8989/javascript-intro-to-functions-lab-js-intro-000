289de3c7c268bd42c44c0e20d373d507f8ddbf06
chatty-forum-3938 <evanshepard1208@gmail.com>
2021-04-16 12:16:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ var mixedCase = "Hi there"
 function sayHiToGrandma(string) {
   if (string.toUpperCase === string) {
     return "YES INDEED!"
+  } else if (string.toLowerCase === string) {
+    return "I can't hear you!"
   } else {
     return "I can't hear you!"
   }
